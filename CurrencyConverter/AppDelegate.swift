f51174b905317964593ff3d358da3836217eaa3f
zhi.zhong <zhong.zhi@163.com>
2020-08-21 16:44:12 +0800

    chore: 禁用引导页

--- CurrencyConverter/AppDelegate.swift
+++ CurrencyConverter/AppDelegate.swift
@@ -19,12 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		IwatchSessionUtil.shareManager.startSession()
 		
 		//这里判断是否第一次启动APP
-		if (!(UserDefaults.standard.bool(forKey: "everLaunched"))) {
-			UserDefaults.standard.set(true, forKey:"everLaunched")
-			let guideViewController = GuideViewController()
-			self.window!.rootViewController = guideViewController
-			print("guideview launched!")
-		}
+//        窗口高度有问题，先注释
+//		if (!(UserDefaults.standard.bool(forKey: "everLaunched"))) {
+//			UserDefaults.standard.set(true, forKey:"everLaunched")
+//			let guideViewController = GuideViewController()
+//			self.window!.rootViewController = guideViewController
+//			print("guideview launched!")
+//		}
 		
         return true
     }
